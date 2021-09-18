6ee06a900cfa81e451e4eaf0dd9c3738578b29e1
shyamtarams <shyamkrishnanpv535@gmail.com>
2021-09-18 14:39:13 +0530

    updated settings

--- student/settings.py
+++ student/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',#adding middleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
